1f6da86b8704bc1d25e71346ae76841bd7f7b9c4
nwmarinoto <nwmarinoto@gmail.com>
2024-11-11 13:29:47 -0500

    add compilerflags skeleton

--- artus/include/Core/Driver.h
+++ artus/include/Core/Driver.h
@@ -1,9 +1,23 @@
 #ifndef ARTUS_CORE_DRIVER_H
 #define ARTUS_CORE_DRIVER_H
 
+#include "llvm/Target/TargetMachine.h"
+
 namespace artus {
 
 
+struct CompilerFlags {
+
+};
+
+/// Logic driver for the compilation process.
+class Driver {
+  /// The target machine for the current host.
+  llvm::TargetMachine *TM;
+
+public:
+  Driver(const CompilerFlags &flags);
+};
 
 } // namespace artus
 
